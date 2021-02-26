f182240490ee122d94486ca0b4603cd04fb9d43f
Jussi Asp <jussi.asp@helsinki.fi>
2021-02-26 13:10:15 +0200

    autocommit

--- test_python/handle_image_with_tesseract.py
+++ test_python/handle_image_with_tesseract.py
@@ -20,19 +20,22 @@ import pymysql
 image_file=sys.argv[1]
 page_id=sys.argv[2]
 
-# print(f'saatiin argumentti:{image_file}')
-# print(f'saatiin argumentti:{page_id}')
+# print("The arguments are: " , str(sys.argv))
+# print(f'saatiin argumentt image_file:{image_file}')
+# print(f'saatiin argumentti page_id:{page_id}')
 
 env_path = Path('.') / '.env'
 load_dotenv(dotenv_path=env_path, verbose=True)
 
 # print(f'minä olen {os.getenv("USER")}')
 
-# create database connection
+# create database connection # TODO: use pool?
 connection = pymysql.connect(host=os.getenv("DATABASE_HOST"),
                          user=os.getenv("DATABASE_USER"),
                          password=os.getenv("DATABASE_PASSWORD"),
-                         db=os.getenv("DATABASE_NAME"))
+                         db=os.getenv("DATABASE_NAME")
+                         )
+connection.autocommit(True) # TODO: to autocommit or not?
 cursor=connection.cursor()
 
 def persist_document(image_file):
@@ -40,7 +43,6 @@ def persist_document(image_file):
     values = (image_file)
     cursor.execute(sql, values)
     new_id=cursor.lastrowid
-    connection.commit()
     return new_id
 
 document_id = persist_document(image_file)
@@ -50,7 +52,7 @@ document_id = persist_document(image_file)
 
 tesserakti.OMP_THREAD_LIMIT=1   # TODO: testaa
 
-data=tesserakti.image_to_data(image_file, output_type=Output.DATAFRAME, config="--dpi 600 -l eng") 
+data=tesserakti.image_to_data(image_file, output_type=Output.DATAFRAME, config="--dpi 600 -l eng")
 
 data.fillna(value="", inplace=True) # replace NaN words with "" TODO: drop these? or something?
 
@@ -62,31 +64,26 @@ def persist_page(row):
     sql = f"INSERT INTO tes_page (id, document_id, {common_columns}) VALUES ({'%s,'*(6-1)}%s)"
     values = (page_id, document_id, row.left, row.top, row.width, row.height)
     cursor.execute(sql, values)
-    connection.commit()
 
 def persist_block(row):
     sql = f"INSERT INTO tes_block (id, page_id, document_id, {common_columns}) VALUES ({'%s,'*(7-1)}%s)"
     values = (row.block_num, page_id, document_id, row.left, row.top, row.width, row.height)
     cursor.execute(sql, values)
-    connection.commit()
 
 def persist_paragraph(row):
     sql = f"INSERT INTO tes_paragraph (id, block_id, page_id, document_id, {common_columns}) VALUES ({'%s,'*(8-1)}%s)"
     values = (row.par_num, row.block_num, page_id, document_id, row.left, row.top, row.width, row.height)
     cursor.execute(sql, values)
-    connection.commit()
 
 def persist_line(row):
     sql = f"INSERT INTO tes_line (id, paragraph_id, block_id, page_id, document_id, {common_columns}) VALUES ({'%s,'*(9-1)}%s)"
     values = (row.line_num, row.par_num, row.block_num, page_id, document_id, row.left, row.top, row.width, row.height)
     cursor.execute(sql, values)
-    connection.commit()
 
 def persist_word(row):
     sql = f"INSERT INTO tes_word (text, conf, id, line_id, paragraph_id, block_id, page_id, document_id, {common_columns}) VALUES ({'%s,'*(12-1)}%s)"
     values = (row.text, row.conf, row.word_num, row.line_num, row.par_num, row.block_num, page_id, document_id, row.left, row.top, row.width, row.height)
     cursor.execute(sql, values)
-    connection.commit()
 
 for row in data.itertuples(index = True, name ='Area'): 
     tyyppi=row.level
@@ -101,6 +98,7 @@ for row in data.itertuples(index = True, name ='Area'):
     if (tyyppi == 5):
         persist_word(row)
 
+# connection.commit()
 connection.close()
 
 print("DONE.")
